3af474717596cd601dec69a35f9b557271abd231
moonsng <ericajg324@naver.com>
2023-06-30 23:09:45 +0900

    learning jsx

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+let posts = "오늘의 일기1"
+let date = "2023-06-30"
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code> 리액트로 웹앱 만들기 시작!</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>장문성의 개발일지</div>
+      </div>
+      <div className="list">
+      <h4>{posts}</h4>
+      <p>6월 30일 발행</p>
+      </div>
+     
     </div>
   );
 }
