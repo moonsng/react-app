fee835d5e4397f5e0127874aa56bb3b0df6944e5
moonsng <ericajg324@naver.com>
2023-07-02 17:39:16 +0900

    learned map and props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,12 +5,12 @@ import React, { useState } from 'react';
 
 function App() {
 
-  let [title, button] = useState(['일기쓰는 앱을 만들까', '포트폴리오를 만들까', '그냥영화사이트?']);
-  let [따봉, 변경용] = useState(0);
+  let [title, setButton] = useState(['일기쓰는 앱을 만들까', '포트폴리오를 만들까', '그냥영화사이트?']);
+  let [따봉, 변경용] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false); //ui의 현재 상태를 state로 저장한다.
+  let [모달제목, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
-  function 함수() {
-    변경용(따봉 + 1)
-  }
 
   return (
     <div className="App">
@@ -18,34 +18,57 @@ function App() {
         <h4>장문성의 개발 일지</h4>
       </div>
 
-      <button onClick={()=>{
+      <button onClick={() => {
         let desc = [...title];
-        desc.sort(); 
-        button(desc)
-        console.log(desc)
+        desc.sort();
+        setButton(desc)
       }}>가나다순정렬</button>
 
       <button onClick={() => {
         let copy = [...title];
-        copy[0] ='여자코트';
-        button(copy)
+        copy[0] = '여자코트 추천';
+        setButton(copy)
       }}> 수정버튼 </button>
 
-      <div className="list">
-        <h4>{title[0]} </h4><span onClick={함수}>❤️</span> {따봉}
-        <p>7월 1일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>7월 1일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>7월 1일 발행</p>
-      </div>
 
-    </div>
+      {
+        title.map(function (a, i) {
+          return (<div className="list" key={i}>
+            <h4 onClick={() => { setModal(true); setTitle(i); }} > {title[i]}</h4>
+            <span onClick={() => {
+              let copy = [...따봉]; copy[i] = copy[i] + 1; 변경용(copy)
+            }}>❤️</span> {따봉[i]}
+            <p>7월 1일 발행</p>
+            <button onClick={()=>{let sak = [...title]; sak.splice(i,1); setButton(sak);}}>삭제</button>
+          </div>)
+        })
+      }
+
+      <input onChange={(e) => {
+        입력값변경(e.target.value)
+      }} ></input>
+      <button onClick={() => {
+        let newTitle = [...title];
+        newTitle.unshift(입력값);
+        setButton(newTitle)
+      }}>글발행</button>
+      {modal == true ? <Modal 모달제목={모달제목} title={title} 수정버튼={setButton} /> : null}
+
+    </div >
   );
+}
+
+
+//컴포넌트
+function Modal(props) {
+  return (
+    <div className='modal'>
+      <h4> {props.title[props.모달제목]} </h4>
+      <p>date</p>
+      <p>detail contents</p>
+      <button onClick={() => { props.수정버튼(['여자코트']) }}>수정버튼</button>
+    </div>
+  )
 }
 
 export default App;
